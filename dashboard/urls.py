50051cc8a1373f6344541d6551cb87a1f6392a9d
Pedro de Borba <pedrodeborba1709@gmail.com>
2024-04-14 20:44:10 -0300

    :art: create main routes

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -2,6 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.index, name='index'),
-   path('test/', views.test, name='test')
+   path('', views.dashboard, name='index'),
+   path('admins/', views.admins, name='admins'),
+   path('equipamentos/', views.equipamentos, name='equipamentos'),
+   path('operadores/', views.operadores, name='operadores'),
+   path('ordens/', views.ordens, name='ordens'),
+   path('categorias/', views.categorias, name='categorias'),
+   path('emprestimos/', views.emprestimos, name='emprestimos'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,8 +3,23 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("Initial page")
+def dashboard(request):
+    return render(request, 'dashboard/index.html')
 
-def test(request):
-    return HttpResponse("Test page")
+def admins(request):
+    return render(request, 'links/admins.html')
+
+def operadores(request):
+    return render(request, 'links/operadores.html')
+
+def equipamentos(request):
+    return render(request, 'links/equipamentos.html')
+
+def ordens(request):
+    return render(request, 'links/ordens.html')
+
+def categorias(request):
+    return render(request, 'links/categorias.html')
+
+def emprestimos(request):
+    return render(request, 'links/emprestimos.html')
