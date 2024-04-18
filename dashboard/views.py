260c42d3305bc127da8135b188eca424a7166706
pedrodeborba <pedrodeborba1709@gmail.com>
2024-04-18 17:04:57 -0300

    :hammer: structuring code

--- dashboard/views.py
+++ dashboard/views.py
@@ -4,22 +4,34 @@ from django.http import HttpResponse
 # Create your views here.
 
 def dashboard(request):
-    return render(request, 'dashboard/index.html')
+    return render(request, 'main/dashboard/index.html')
 
 def admins(request):
-    return render(request, 'links/admins.html')
+    admin = 5
+    context = {'admin': range(admin)}
+    return render(request, 'main/admins/index.html', context)
 
 def operadores(request):
-    return render(request, 'links/operadores.html')
+    operador = 5
+    context = {'operador': range(operador)}
+    return render(request, 'main/operadores/index.html', context)
 
 def equipamentos(request):
-    return render(request, 'links/equipamentos.html')
+    equipamento = 5
+    context = {'equipamento': range(equipamento)}
+    return render(request, 'main/equipamentos/index.html', context)
 
 def ordens(request):
-    return render(request, 'links/ordens.html')
+    ordem = 5
+    context = {'ordem': range(ordem)}
+    return render(request, 'main/ordens/index.html', context)
 
 def categorias(request):
-    return render(request, 'links/categorias.html')
+    categoria = 5
+    context = {'categoria': range(categoria)}
+    return render(request, 'main/categorias/index.html', context)
 
 def emprestimos(request):
-    return render(request, 'links/emprestimos.html')
+    emprestimo = 5
+    context = {'emprestimo': range(emprestimo)}
+    return render(request, 'main/emprestimos/index.html', context)
