02ef5989e1d38c7b91dd896b7a0510c35ac4b933
pedrodeborba <pedrodeborba1709@gmail.com>
2024-07-03 14:51:27 -0300

    ci: :bricks: Config site admin

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
+from .models import Admin
 
-# Register your models here.
+admin.site.site_header = 'TI - Usaflex'
+
+class AdminAdm(admin.ModelAdmin):
+    list_display = ('usuario', 'email', 'cracha', 'cargo')
+    list_filter = ['cargo']
+
+#Register your models here
+admin.site.unregister(Group)
+admin.site.register(Admin, AdminAdm)
+
--- dashboard/models.py
+++ dashboard/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Admin(models.Model):
+    usuario = models.CharField(max_length=100, null=True)
+    cracha = models.IntegerField(null=True)
+    senha = models.CharField(max_length=128, null=True)
+    email = models.EmailField(max_length=254, null=True)
+    cargo = models.CharField(max_length=100, null=True)
+
+    class Meta:
+        verbose_name="Admin"
+        verbose_name_plural="Admins"
+
