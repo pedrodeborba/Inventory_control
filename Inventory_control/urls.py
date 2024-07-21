a9925a37b7eeb4ade3b6d9ee7c9cb52941acc6c5
pedrodeborba <pedrodeborba1709@gmail.com>
2024-07-21 12:20:05 -0300

    feat: :sparkles: user profile

--- Inventory_control/urls.py
+++ Inventory_control/urls.py
@@ -13,5 +13,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='user-login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'), name='user-logout'),
     
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
